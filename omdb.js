553343f9c70a1edaaff4eabe620c8d57a116b7f1
michael nauman <bodhiandmike@gmail.com>
2015-01-29 10:03:04 -0800

    replaced for loop with a forEach

--- omdb.js
+++ omdb.js
@@ -3,16 +3,15 @@ $(document).ready(function() {
 	$("form").on("submit", function(event) {
 		event.preventDefault();
 		var test = event.target.movieName.value;
-		console.log(test);
 
 		$.ajax({
 			url: "http://www.omdbapi.com/?s=" + test,
+			dataType : "json",
 			success: function (data){
-				console.log(data);
-				$("body").append("<h2>" + data + "</h2>")			
-
-				// this is where the bonus ajax would go
-
+				data.Search.forEach(function(value) {
+					console.log(value);
+					$("body").append("<h4>" + value.Title + "</h4>");
+				});
 			}
 		});
 
